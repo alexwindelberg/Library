0d37bf4ed6e904cc30e3ceccc50eed6407bbed5b
alexander windelberg <alexwindelberg@gmail.com>
2020-03-03 20:19:32 -0500

    Changed id to GraphID type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql');
 const _ = require('lodash');
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
 // dummy data
 var books = [
@@ -14,7 +14,7 @@ var books = [
 const BookType = new GraphQLObjectType({
     name      : 'Book',
     fields    : () => ({
-        id    : { type: GraphQLString },
+        id    : { type: GraphQLID },
         name  : { type: GraphQLString },
         genre : { type: GraphQLString },
     })
@@ -23,14 +23,16 @@ const BookType = new GraphQLObjectType({
 
 // Root Queries are the initial queries
 // They're the queries that are at ran at init and tie us to
-// other relationship
+// other schema relationships
 const RootQuery = new GraphQLObjectType ({
     name     : 'RootQueryType',
     fields   : {
-        // On the front-end when querying this label is the keyword we're querying for
+        // On the front-end when querying this label is the keyword we're using to query
+        // the book query accepts an id of GraphQLID type and uses it retrieve the requested
+        // book from the BookType then returns it's result
         book : {
             type : BookType,
-            args : { id : { type : GraphQLString } },
+            args : { id : { type : GraphQLID } }, // we are changing from GraphQLString to ID type
             resolve (parent, args) {
                 // code to get the data from the db / other sources 
                 // using lodash
@@ -39,8 +41,6 @@ const RootQuery = new GraphQLObjectType ({
         }
     }
 });
-
-
 
 
 

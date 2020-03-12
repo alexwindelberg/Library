79feb9c9d709cd94453d433dbbdd6dd519cc80db
alexander windelberg <alexwindelberg@gmail.com>
2020-03-11 21:29:43 -0400

    adding mutation queries and pushing them to mongolab

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 const Book = require('../models/book');
-const Author= require('../models/author');
+const Author = require('../models/author');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
 
@@ -83,8 +83,35 @@ const RootQuery = new GraphQLObjectType ({
     }
 });
 
+const Mutation = new GraphQLObjectType({
+    name : 'Mutation',
+    fields: {
+        addAuthor : {
+            type : AuthorType,
+            args : {
+                name : { type : GraphQLString },
+                age  : { type : GraphQLInt },
+            },
+            resolve (parent, args) {
+                let author = new Author({
+                    name : args.name,
+                    age  : args.age,
+                });
 
+                return author.save();
+            }
+        },
+        addBook : {
+            type : BookType,
+            args : {
+                name  : { type : GraphQLString },
+                genre : { type : GraphQLString } 
+            }
+        }
+    }
+});
 
 module.exports = new GraphQLSchema({
-    query:RootQuery
+    query : RootQuery,
+    mutation : Mutation,
 });

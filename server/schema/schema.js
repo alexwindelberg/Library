1a587f05b501ca96d16678398a2e1d2bb9923363
alexander windelberg <alexwindelberg@gmail.com>
2020-03-12 22:51:38 -0400

    added restrictions to fields

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,9 +3,9 @@ const _ = require('lodash');
 const Book = require('../models/book');
 const Author = require('../models/author');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList, GraphQLNonNull } = graphql;
 
-// This is creating the schema in graphQL
+// This is creating the schema in graphQL 
 const AuthorType = new GraphQLObjectType({
     name      : 'Author',
     fields    : () => ({
@@ -95,24 +95,23 @@ const Mutation = new GraphQLObjectType({
         addAuthor : {
             type : AuthorType,
             args : {
-                name : { type : GraphQLString },
-                age  : { type : GraphQLInt },
+                name : { type : new GraphQLNonNull(GraphQLString) },
+                age  : { type : new GraphQLNonNull(GraphQLInt) },
             },
             resolve (parent, args) {
                 let author = new Author({
                     name : args.name,
                     age  : args.age,
                 });
-
                 return author.save();
             }
         },
         addBook : {
             type : BookType,
             args : {
-                name     : { type : GraphQLString },
-                genre    : { type : GraphQLString },
-                authorId : { type : GraphQLID }
+                name     : { type : new GraphQLNonNull(GraphQLString) },
+                genre    : { type : new GraphQLNonNull(GraphQLString) },
+                authorId : { type : new GraphQLNonNull(GraphQLID) }
             },
             resolve (parent, args) {
                 let book = new Book({

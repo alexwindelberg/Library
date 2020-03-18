a20262b114e5b4eb9ca2ca9c79e11910d2eda51c
alexander windelberg <alexwindelberg@gmail.com>
2020-03-17 22:22:58 -0400

    retriving data from the DB and beginning to display responses to the front page

--- client/src/component/BookList.js
+++ client/src/component/BookList.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
 
-
 const getBooksQuery = gql`
     {
         books{
@@ -12,18 +11,33 @@ const getBooksQuery = gql`
     }
 `
 
-
 class BookList extends Component {
-  render () {
-      console.log(this.props);
-    return (
-      <div>
-          <ul id="book-list">
-              <li> Book Name </li>
-          </ul>
-      </div>
-    );
-  }
+
+    displayBooks() {
+        // These props have been set here by the graphql, you can see it when you load the
+        // page and check the console. An object will be given this object will also contain a
+        // loading property, once complete it will return a books array
+        let data = this.props.data;
+        if(data.loading) {
+            return ( <div> Loading Books...</div> )
+        }
+        else {
+            return data.books.map( book => {
+                return ( <li>{book.name}</li> );
+            });
+        }
+    }
+
+    render () {
+        console.log(this.props);
+        return (
+        <div>
+            <ul id="book-list">
+                <li> Book Name </li>
+            </ul>
+        </div>
+        );
+    }
 }
 
 export default graphql(getBooksQuery)(BookList);

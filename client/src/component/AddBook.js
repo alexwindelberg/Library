3e26cd3d2b759273c66338a7ca929afa68731c64
alexander windelberg <alexwindelberg@gmail.com>
2020-03-17 22:39:30 -0400

    added new page for adding books

---
+++ client/src/component/AddBook.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getAuthorsQuery = gql`
+    {
+        authors{
+            name
+            id
+        }
+    }
+`
